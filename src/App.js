301d89af1cf462cb7bc4f53aae913ad4d2079e43
jahidul <programmerjahid162@gmail.com>
2022-06-01 17:51:37 +0600

    login and registration design complate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
+import Home from "./Pages/Home";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Home />} />
+          {/* Home */}
+          <Route path="/home" element={<Home />} />
+          {/* Login */}
+          <Route path="login" element={<Login />} />
+          {/* Registration  */}
+          <Route path="registration" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Login.js
@@ -0,0 +1,115 @@
+import React from "react";
+
+import loginImages from "../images/5035121.jpg";
+
+import "../shared/menu.css";
+import { Form, Input, Button, Divider } from "antd";
+import { Row, Col, Image } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const registration = useNavigate();
+  return (
+    <>
+      <Row style={{ overflowX: "hide" }}>
+        <Col
+          id="signInmain"
+          xs={{ span: 22, offset: 0 }}
+          lg={{ span: 16, offset: 4 }}
+          md={{ span: 16, offset: 4 }}
+          sm={{ span: 22, offset: 0 }}
+        >
+          {/* <h2 style={{marginTop:30, textAlign: "center" , marginLeft:20}}>Login </h2> */}
+          <Row
+            gutter={[36]}
+            style={{
+              // border: "2px solid gray",
+              display: "flex",
+              alignItems: "center",
+              // padding: 3,
+            }}
+          >
+            <Col
+              xs={{ span: 24, offset: 0 }}
+              lg={{ span: 10, offset: 0 }}
+              md={{ span: 10, offset: 0 }}
+              sm={{ span: 24, offset: 0 }}
+            >
+              <Image
+                style={{ padding: 20 }}
+                width="100%"
+                src={loginImages}
+                preview={false}
+              />
+            </Col>
+
+            <Col lg={{ span: 2, offset: 0 }} md={{ span: 2, offset: 0 }}>
+              <Divider
+                id="signinDeviderVerticle"
+                type="vertical"
+                style={{ height: "350px", backgroundColor: "#4B9FFF" }}
+              />
+
+              <Divider
+                id="signinDeviderHorizonal"
+                type="horizonal"
+                style={{
+                  width: "375px",
+                  backgroundColor: "#4B9FFF",
+                }}
+              />
+            </Col>
+
+            <Col
+              xs={{ span: 24, offset: 0 }}
+              lg={{ span: 10, offset: 0 }}
+              md={{ span: 10, offset: 0 }}
+              sm={{ span: 24, offset: 0 }}
+              style={{ marginTop: 35 }}
+            >
+              <Form
+                name="wrap"
+                labelCol={{ flex: "110px" }}
+                labelAlign="left"
+                labelWrap
+                wrapperCol={{ flex: 1 }}
+                colon={false}
+              >
+                <Form.Item
+                  label="Email"
+                  name="username"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item style={{ float: "right", overflowX: "hide" }}>
+                  <Button type="primary" htmlType="submit">
+                    Login
+                  </Button>
+
+                  <br></br>
+                  <Form.Item style={{ color: "#007DFE" }}>
+                    <a onClick={() => registration("/registration")}>
+                      Create New Account?
+                    </a>
+                  </Form.Item>
+                </Form.Item>
+              </Form>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Login;
