55366e59ff3e3b8525794b26029adb5d5e23d4fe
Md Ikbal Razu <37942730+ikbalrazu@users.noreply.github.com>
2022-06-08 20:45:38 +0600

    login and registration done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./Pages/Login";
 import Registration from "./Pages/Registration";
 import Home from "./Pages/Home";
 import EmailOTPVerify from "./Pages/EmailOTPVerify";
-
+import Test from "./test/Test";
 
 function App() {
 
@@ -20,8 +20,9 @@ function App() {
           <Route path="/" element={<Login />} />
           {/* <Route path="/login" element={<Login/>}/> */}
           <Route path="/registration" element={<Registration />} />
-          <Route path="emailotpverify" element={<EmailOTPVerify/>}/>
+          {/* <Route path="emailotpverify" element={<EmailOTPVerify/>}/> */}
           <Route path="/home" element={<Home/>} /> 
+          {/* <Route path="/test" element={<Test/>} /> */}
         </Routes>
       
     </div>
